0ea6219b1e771e2e0f63351d8ea9117f58388b0b
B V K Prasanth <krishnaprasanth.bv@gmail.com>
2023-11-26 14:54:56 -0500

    Imports Removed in ProductController

--- src/main/java/com/kp/ProductService/controller/ProductController.java
+++ src/main/java/com/kp/ProductService/controller/ProductController.java
@@ -2,13 +2,11 @@ package com.kp.ProductService.controller;
 
 import com.kp.ProductService.model.ProductRequest;
 import com.kp.ProductService.model.ProductResponse;
-import com.kp.ProductService.repository.ProductRepository;
 import com.kp.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
